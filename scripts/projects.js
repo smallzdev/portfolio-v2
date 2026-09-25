352e56e070bdc00ced714000ab378e5bf929c62b
smallz <233221808+smallzdev@users.noreply.github.com>
2026-09-25 11:20:53 +1000

    Date Update + Visual
    1. The date now fetches from github on every project + main project page! This took me a while, but I'm so happy that I did it!
    
    2. I fixed and made the text more spread out for the divs on projects.html.
    
    3. Added "Made with ❤️ by Smallz :D" on every page with a break to seperate it from the main content.
    
    4. More!

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,7 +1,23 @@
-fetch('https://api.github.com/repos/smallzdev/solar-os/commits?per_page=1')
-    .then(data => {
-        var lastUpdated = data.commit.date
-        print(lastUpdated)
-        var gitDate = document.querySelector('#gitDate')
-        gitDate.textContent = lastUpdated
-    })
+// Scripts connected to projects.html and every other project page :D
+
+// Fetches the data from github
+
+function dateUpdate(repo, updatetext) { 
+    fetch(`https://api.github.com/repos/smallzdev/${repo}/commits?per_page=1`)
+        .then(response => response.json())
+        .then(data => {
+            var lastUpdated = new Date(data[0].commit.committer.date).toLocaleDateString();
+            console.log(lastUpdated)
+            var gitDate = document.querySelector(updatetext)
+            gitDate.textContent = "Last Updated: " + lastUpdated
+        })
+    .catch(error => console.log(error))
+};
+
+dateUpdate('godot-thirdspace', '#thirdspacegodot-date')
+dateUpdate('solar-os', '#solaros-date')
+dateUpdate('flight-sim-hackpad', '#hackpad-date')
+dateUpdate('portfolio','#portfoliov1-date')
+dateUpdate('portfolio-v2', '#portfoliov2-date')
+dateUpdate('yaknow', '#yaknow-date')
+dateUpdate('bakebuild', '#bakebuild-date')
